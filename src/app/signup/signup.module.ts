2bd8e3b4d01c492f3bbc05548c5d577dd6537843
Pamela Cabezas <cabezas.pam@gmail.com>
2020-05-03 12:04:58 -0300

    Adição dos módulos do Angular Material

--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountComponent, BankComponent, GoldComponent, PassComponent } from "./signup.index";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
 @NgModule({
 	declarations: [AccountComponent, BankComponent, GoldComponent, PassComponent],
   imports: [
-    CommonModule
+	CommonModule,
+	MatFormFieldModule,	
+	MatInputModule,
+    MatButtonModule,
   ]
 })
 export class SignupModule { }
