544a62079916ab47216ba9502609ba3dba68e4fe
phmxavier <phmxavier@gmail.com>
2020-05-03 01:58:02 -0300

    Inclusão do botão para conectar ao open banking

--- src/app/signup/bank/bank.component.ts
+++ src/app/signup/bank/bank.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BankComponent implements OnInit {
 
+  myWindow: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  connectToBank(event: Event){
+    this.myWindow = window.open("https://localhost:32778/?clientId=123", "", "toolbar=no,scrollbars=no,resizable=no,width=400,height=600");
+    this.myWindow.onunload=function(){
+      alert("Janela fechada");
+    };
   }
 
 }
