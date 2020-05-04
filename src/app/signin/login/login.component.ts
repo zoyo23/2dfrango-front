a387a7c18eca98293308bbf140b9d5e1556ab1f8
phmxavier <phmxavier@gmail.com>
2020-05-03 23:17:18 -0300

    Chamada do método de validação do JWT do google

--- src/app/signin/login/login.component.ts
+++ src/app/signin/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { AuthService, SocialUser, GoogleLoginProvider, FacebookLoginProvider } from 'angularx-social-login';
 
@@ -11,11 +12,22 @@ export class LoginComponent implements OnInit {
 
   user: SocialUser;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private http: HttpClient) { }
 
   ngOnInit() {
     this.authService.authState.subscribe((user) => {
       this.user = user;
+      if (this.user != null) {
+        this.http.get("https://localhost:5001/Autenticacao/verificaLogin", {
+          headers: new HttpHeaders({
+            'Authorization': this.user.idToken
+          })
+        })
+          .subscribe((data: any) => {
+            console.dir("Resposta da API");
+            console.dir(data);
+          });
+      }
     });
   }
 
